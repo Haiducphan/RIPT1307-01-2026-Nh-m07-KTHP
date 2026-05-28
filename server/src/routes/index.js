2d19cd8afd330dc972facffa0e361f9fe616af32
truonghieu3111 <hieutruong3107@gmail.com>
2026-05-28 10:39:50 +0700

    feat: add time-based statistics API - monthly chart and overdue list

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const authRoutes = require('./auth.routes');
 const borrowRequestRoutes = require('./borrowRequests.routes');
 const equipmentRoutes = require('./equipment.routes');
+const statisticsRoutes = require('./statistics.routes');
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/equipment', equipmentRoutes);
 router.use('/borrow-requests', borrowRequestRoutes);
+router.use('/statistics', statisticsRoutes);
 
 module.exports = router;

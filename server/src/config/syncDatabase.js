a0dd8818dbb014512f6ad4deb937cb04a4d76791
truonghieu3111 <hieutruong3107@gmail.com>
2026-06-07 21:57:54 +0700

    feat: update before merge to main

--- server/src/config/syncDatabase.js
+++ server/src/config/syncDatabase.js
@@ -5,8 +5,6 @@ async function syncDatabase() {
   await sequelize.authenticate();
   console.log('Ket noi Database thanh cong!');
 
-  await Equipment.sync({ alter: true });
-  console.log('Sync Equipment table thanh cong!');
 }
 
 module.exports = { syncDatabase };
